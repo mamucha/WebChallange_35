fb60fbe0255e1c620cd1997123de85393ec5ae04
Marcin <marcin9213@gmail.com>
2020-02-17 18:43:41 +0100

    count up animation

--- src/js/vendor/script.js
+++ src/js/vendor/script.js
@@ -54,4 +54,31 @@ $(document).scroll(() => {
 })
 
 
-// buttons
+// count up animation
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 980) {
+        $('.c-counter__number').each(function () {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                    countNum: countTo
+                },
+
+                {
+
+                    duration: 3000,
+                    easing: 'linear',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum);
+                    }
+
+                });
+        });
+    }
+});
